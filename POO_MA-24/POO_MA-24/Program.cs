a1113ec8afd4a476d34b3e83ebe69f1d5b032ccd
CODURI Luca <Luca.CODURI@cpnv.ch>
2019-04-04 10:48:14 +0200

    Création des différentes class

---
+++ POO_MA-24/POO_MA-24/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO_MA_24
+{
+    
+    class Program
+    {
+        public static Random rand = new Random();
+        
+
+        public class Monstre
+        {
+            public bool estVivant { get; private set; }
+            public int puissance { get; private set; }
+            public Monstre()
+            {
+                estVivant = true;
+            }
+            public void SubitDegats()
+            {
+                estVivant = false;
+            }
+            public int LancerDe()
+            {
+                return rand.Next(1, 7);
+            }
+            public virtual void Attaque(Joueur joueur)
+            {
+                if(this.LancerDe() > joueur.LancerDe())
+                {
+                    joueur.SubitDegats(puissance);
+                }
+            }
+        }
+        public class MonstreDifficile : Monstre
+        {
+            private const int degatsSort = 5;
+
+            public MonstreDifficile()
+            {
+
+            }
+            public override void Attaque(Joueur joueur)
+            {
+                base.Attaque(joueur);
+                joueur.SubitDegats(SortMagique());
+            }
+            public int SortMagique()
+            {
+                int lance = this.LancerDe();
+                if (lance == 6 || lance == 5) return 0;
+                return this.LancerDe() * degatsSort;
+            }
+        }
+        public class Joueur
+        {
+            public int pointsVie { get; private set; }
+
+            public Joueur(int pointsVie)
+            {
+                this.pointsVie = pointsVie;
+            }
+            public void SubitDegats(int points)
+            {
+                if (!BouclierFonctionne())
+                {
+                    this.pointsVie -= points;
+                }
+            }
+            public bool BouclierFonctionne()
+            {
+                return rand.Next(1, 7) <= 2;
+            }
+            public int LancerDe()
+            {
+                return rand.Next(1, 7);
+            }
+            public void Attaque(Monstre monstre)
+            {
+
+                if(this.LancerDe() > monstre.LancerDe())
+                {
+                    monstre.SubitDegats();
+                }
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            Joueur joueur = new Joueur(150);
+            Monstre monstre = new Monstre();
+            MonstreDifficile difficile = new MonstreDifficile();
+
+            Console.WriteLine(joueur.LancerDe());
+            Console.WriteLine(monstre.LancerDe());
+            Console.WriteLine(difficile.LancerDe());
+
+            while (true)
+            {
+
+            }
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,14 @@
+# POO
+Ce projet demandé par notre professeur du cours MA-24 a pour but de nous noter sur notre capacité a utiliser Github ainsi que notre capacité à se documenter afin de résoudre un problème au quel nous sommes confronté.
+Pour ce projet, j'ai choisi comme sujet la programmation orientée objet.
+
+## Etape 1 :
+Trouver au minimum un site que je pense être bien et qui pourra m'aider dans la compréhension du sujet.
+
+[OpenClassRoom](https://openclassrooms.com/fr/courses/2818931-programmez-en-oriente-objet-avec-c/2818971-la-poo-et-le-c) Site qui explique très bien le sujet avec des exemples.
+
+## Etape 2 :
+Lire le tutoriel et essayer de reproduire ce qui m'est expliqué.
+
+## Etape 3 :
+Trouver et démontrer les avantages de l'utilisation de la programmation orientée objet à l'aide d'un petit programme commenté.
